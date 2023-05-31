86994e7d4032945c60a38bf567eed3bbd1933b40
Okan Demir <okandemir3358@gmail.com>
2023-05-31 08:46:14 +0300

    tested

--- test/unit_tests/point_at_v2.dart
+++ test/unit_tests/point_at_v2.dart
@@ -3,11 +3,27 @@ import 'package:vector_math/vector_math_64.dart';
 import '../testing_tools/testing_tools.dart';
 
 void main() {
-  group('constructor', () {
-    test('constructor with three-entry look up table', () {
-      final lookUpTable = <Vector2>[Vector2(0.0, 0.0), Vector2(2.0, 0.0), Vector2(3.0, 0.0)];
-      final object = EvenSpacer(lookUpTable);
-      expect(object, TypeMatcher<EvenSpacer>());
+  group('pointAtV2', () {
+    test('new pointAtV2 method test', () {
+      var start = Vector2(0, 0);
+      var control1 = Vector2(0, -5000);
+      var control2 = Vector2(10000, -5000);
+      var end = Vector2(10000, 0);
+
+      final curve = CubicBezier([start, control1, control2, end]);
+
+      Vector2 previousPoint = start;
+      double averageDistance = 1392;
+
+      for (double t = 0.1; t <= 1.0; t += 0.1) {
+        var point = curve.pointAtV2(t);
+
+        final distance = point.distanceTo(previousPoint);
+        expect(averageDistance - distance < -10, false);
+        expect(averageDistance - distance > 10, false);
+
+        previousPoint = point;
+      }
     });
   });
 }
